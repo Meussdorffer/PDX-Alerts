b81d38af3e73fc7b2d60eb4dbb363324e5d25cb7
Jack Meussdorffer <jackcmv@gmail.com>
2019-05-20 06:47:52 -0700

    removed more sensitive data

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+import json
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+with open('app/secret.json') as f:
+	secret = json.loads(f.read())
+
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'do you ever feel like a plastic bag'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or secret['FLASK_SECRET']
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 	 	'sqlite:///' + os.path.join(basedir, 'alerts.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
